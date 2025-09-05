d73258b231447ec1977680dc314ff3799e5ea76f
Darya <ara1015@tut.by>
2025-09-05 16:44:18 +0300

    added def get_movie

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
 
 
-@app.get('/', summary="Главная функция", tags=["Основная функция"])
-def home():
-    return "Hello world!!!"
+movies = [
+    {
+        "id": 1,
+        "title": "Naked gun"
+    },
+    {
+        "id": 2,
+        "title": "Friends"
+    },
+]
+
+@app.get("/movie", tags=["Movie"])
+def my_movie():
+    return movies
+
+@app.get("/movie/{id}", tags=["Movie"])
+def get_movie(movie_id: int):
+    for m in movies:
+        if m["id"] == movie_id:
+            return m
+    raise HTTPException(status_code=404, detail="Movie doesn't found")
+
+# @app.get('/', summary="Главная функция", tags=["Основная функция"])
+# def home():
+#     return "Hello world!!!"
 
 
 if __name__ == "__main__":
